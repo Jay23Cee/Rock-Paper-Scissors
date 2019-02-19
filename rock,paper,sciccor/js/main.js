683bcd741c667cea47b1b1ad78178952ba3ae87c
Jay23Cee <41491633+Jay23Cee@users.noreply.github.com>
2019-02-18 20:50:20 -0800

    First Rock Paper Scissor game using JS
    Needs to make this look better.
    CSS. needs background (maybe an image? or just a solid color)
    Button  would look better with a image.
    + a Scoreboard. Times User wins vs Computer

---
+++ rock,paper,sciccor/js/main.js
@@ -0,0 +1,69 @@
+var Rock = document.getElementById('R');
+if(Rock){
+Rock.addEventListener("click",run);
+};
+
+var Paper = document.getElementById("P");
+if(Paper){
+    Paper.addEventListener("click",run);
+    };
+
+var Scissors = document.getElementById("S");
+if(Scissors){
+    Scissors.addEventListener("click",run);
+    };
+
+
+   
+
+
+var computerChoice;
+var playerChoice;
+
+var player = document.getElementById("player");
+var computer = document.getElementById("computer");
+var outcome = document.getElementById("outcome");
+
+function run() {
+   
+    playerChoice = this.innerText;
+    computerGamble();
+    compare();
+}
+
+function computerGamble() {
+    var dice = Math.random();
+    if (dice <= 0.33) {
+        computerChoice = "Rock";
+    } else if (dice > 0.33 && dice <= 0.66) {
+        computerChoice = "Paper";
+    } else {
+        computerChoice = "Scissors";
+    }
+}
+ 
+function compare() {
+    player.innerHTML = "You have chosen " + playerChoice;
+    computer.innerHTML = "Computer chooses " + computerChoice;
+
+    if (playerChoice == computerChoice) {
+        outcome.innerHTML = "Draw";
+ 		
+    } else if (playerChoice === "Rock" && computerChoice === "Paper") {
+        outcome.innerHTML = "Defeat!";
+    } else if (playerChoice === "Rock" && computerChoice === "Scissors") {
+        outcome.innerHTML = "Victory!";
+ 	
+
+    } else if (playerChoice === "Paper" && computerChoice === "Rock") {
+        outcome.innerHTML = "Victory!";
+    } else if (playerChoice === "Paper" && computerChoice === "Scissors") {
+        outcome.innerHTML = "Defeat!";
+
+
+    } else if (playerChoice === "Scissors" && computerChoice === "Rock") {
+        outcome.innerHTML = "Defeat!";
+    } else if (playerChoice === "Scissors" && computerChoice === "Paper") {
+        outcome.innerHTML = "Victory!";
+    }
+}
