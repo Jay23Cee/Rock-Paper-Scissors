d83ced96ff893da9a10e2c71cd1497a09abd52ea
Jay23Cee <41491633+Jay23Cee@users.noreply.github.com>
2019-03-17 17:00:43 -0700

    Updated JS file
    Score was added.

--- rock,paper,sciccor/js/main.js
+++ rock,paper,sciccor/js/main.js
@@ -1,5 +1,6 @@
 var Rock = document.getElementById('R');
 if(Rock){
+    
 Rock.addEventListener("click",run);
 };
 
@@ -13,19 +14,37 @@ if(Scissors){
     Scissors.addEventListener("click",run);
     };
 
+var playerimage = document.getElementById("playerImg");
+//if(playerimage){
+ //   playerimage.setAttribute("src", "rock.JPG");
+
+ // }
+
+
+var Computerimage = document.getElementById('computerImg');
+
+
 
    
 
 
 var computerChoice;
 var playerChoice;
+var UserScore =document.getElementById("Uscore");
+var CpuScore=document.getElementById("Cscore");
+var Drawscore=document.getElementById("Dscore");
+
+
 
 var player = document.getElementById("player");
 var computer = document.getElementById("computer");
 var outcome = document.getElementById("outcome");
 
+
 function run() {
-   
+
+
+    
     playerChoice = this.innerText;
     computerGamble();
     compare();
@@ -35,35 +54,83 @@ function computerGamble() {
     var dice = Math.random();
     if (dice <= 0.33) {
         computerChoice = "Rock";
+        Computerimage.src="image/rock.JPG";
     } else if (dice > 0.33 && dice <= 0.66) {
         computerChoice = "Paper";
+        Computerimage.src="image/paper.JPG";
+        
     } else {
         computerChoice = "Scissors";
+        Computerimage.src="image/scissors.JPG";
+
     }
 }
  
+
+
+function updatescore(winner){
+
+    winner.trim();
+    if(winner=="user")
+    { 
+        var num = UserScore.innerHTML;
+        num ++;
+        UserScore.innerHTML=num.toString();
+      }else if(winner=="cpu"){ 
+        var num = CpuScore.innerHTML;
+        num ++;
+        CpuScore.innerHTML=num.toString();
+      }
+      else{
+        var num = DrawScore.innerHTML;
+        num ++;
+        DrawScore.innerHTML=num.toString();
+        }
+
+  }
 function compare() {
+    
     player.innerHTML = "You have chosen " + playerChoice;
     computer.innerHTML = "Computer chooses " + computerChoice;
 
-    if (playerChoice == computerChoice) {
-        outcome.innerHTML = "Draw";
- 		
-    } else if (playerChoice === "Rock" && computerChoice === "Paper") {
-        outcome.innerHTML = "Defeat!";
-    } else if (playerChoice === "Rock" && computerChoice === "Scissors") {
-        outcome.innerHTML = "Victory!";
- 	
+    //UserScore("user");
 
-    } else if (playerChoice === "Paper" && computerChoice === "Rock") {
-        outcome.innerHTML = "Victory!";
-    } else if (playerChoice === "Paper" && computerChoice === "Scissors") {
+  
+    if ((playerChoice.trim()==computerChoice.trim())) {
+       outcome.innerHTML = "Draw";
+       updatescore("Draw");
+
+    } else if ((playerChoice.trim() == "Rock" && computerChoice.trim()== "Paper")) {
         outcome.innerHTML = "Defeat!";
+       updatescore("cpu");
+    } else if ((playerChoice.trim() == "Rock" && computerChoice.trim() == "Scissors")) {
+        outcome.innerHTML = "Victory!";
+        updatescore("user");
+
+    } else if ((playerChoice.trim() == "Paper" && computerChoice.trim() == "Rock")) {
+        this.outcome.innerHTML = "Victory!";
+        updatescore("user");
+        
+    } else if ((playerChoice.trim() == "Paper" && computerChoice.trim() == "Scissors")) {
+        outcome.innerHTML = "Defeat!";
+        updatescore("cpu");
 
 
-    } else if (playerChoice === "Scissors" && computerChoice === "Rock") {
+    } else if ((playerChoice.trim() == "Scissors" && computerChoice.trim() =="Rock")) {
         outcome.innerHTML = "Defeat!";
-    } else if (playerChoice === "Scissors" && computerChoice === "Paper") {
-        outcome.innerHTML = "Victory!";
+        updatescore("cpu");
+    } else if ((playerChoice.trim()== "Scissors" && computerChoice.trim() == "Paper")) {
+        this.outcome.innerHTML = "Victory!";
+        updatescore("user");
     }
+
+    //CHANGING THEIMAGESOF PLAYER CHOICE.
+    var changeimage = playerChoice;
+    changeimage = changeimage.toLowerCase()+".JPG";
+   
+
+    playerimage.src="image/"+changeimage;
+
+ 
+
 }
